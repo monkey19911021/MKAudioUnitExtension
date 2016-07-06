35df2cf3f1bdd22e73de0c15b3abf2aa416ad16d
jiahao <monkey19911021@gmail.com>
2016-07-06 10:59:06 +0800

    参考链接

--- MKAudioUnitExtension/MKAudioUnitExtension/ViewController.m
+++ MKAudioUnitExtension/MKAudioUnitExtension/ViewController.m
@@ -25,3 +25,9 @@
 }
 
 @end
+
+
+//http://www.jianshu.com/p/5b0fa403b3ce
+//http://www.jianshu.com/p/5d18180c69b8
+//http://albert-oma.blogspot.hk/2014/04/ios-audio-unit.html
+//https://zonble.gitbooks.io/kkbox-ios-dev/content/audio_apis/audio_unit_graph.html
